052fe79b5f88d31ec9ec07fe42e1b5cf50e8e65a
Arijan <arijan.elmazi1@gmail.com>
2022-07-11 08:40:03 +0200

    a start

---
+++ Program.fsx
@@ -0,0 +1,17 @@
+namespace Stackcalculator
+
+open System 
+
+type Stack = float list 
+
+module Parsing =
+   type Commands = string list 
+   type Operation 
+   
+   // add more commands if needed 
+   let commands : Commands = ["push"; "pop"; "add"; "sub"; "mul"; "div"] 
+
+
+   let readline : Operation = 
+
+module Evaluation = 
